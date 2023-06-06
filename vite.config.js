bae962fbbc3efd6d79fdc8f16aa5c502c728fc3a
Mauro <salgadom7503@gmail.com>
2023-06-07 00:56:35 +0100

    new

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import jsconfigPaths from 'vite-jsconfig-paths'
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react()],
-  };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    target: "es2015"
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: "./src/setupTests.js"
+  },
+  plugins: [
+    react(),
+    // jsconfigPaths()
+  ],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+})
